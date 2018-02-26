47894bd8a05ddf90c796e698bcd29dd36b030c76
Dileep <rajputd@ufl.edu>
2018-02-26 10:24:51 -0500

    Modify form to send http requests to the specified API

--- testClient.php
+++ testClient.php
@@ -3,26 +3,31 @@
 require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
 
 $client_module = \ExternalModules\ExternalModules::getModuleInstance('redcap_mirth_client', 'v1.0');
-$test_module = new MirthClientTestModule\ExternalModule\ExternalModule($_GET['pid']);
 
-$base_url = $test_module->getProjectSetting('end-point-url');
-$client = $client_module->getClient($base_url);
+if(!empty($_POST['method']) && !empty($_POST['base-url']) ) {
+    $client = $client_module->getClient($_POST['base-url']);
+    $client->request($_POST['method'], $_POST['extension'], $_POST['json']);
+}
 
 ?>
 
-<form method="POST">
+<form method="POST" >
   <div class="form-group">
-    <label for="extension">base url extension</label>
-    <input type="text" class="form-control" id="extension" aria-describedby="extensionHelp" placeholder="extension that will be appended to the base url, ex. /api">
+    <label for="base-url">base url (required)</label>
+    <input type="text" name="base-url" class="form-control" id="method" placeholder="API's base-url, ex. 10.0.2.2:8002/api">
+  </div>
+  <div class="form-group">
+    <label for="method">http method (required)</label>
+    <input type="text" name="method" class="form-control" id="method" placeholder="http method, ex. GET">
+  </div>
+  <div class="form-group">
+    <label for="extension">base url extension (optional)</label>
+    <input type="text" name="extension" class="form-control" id="extension" aria-describedby="extensionHelp" placeholder="extension that will be appended to the base url, ex. /channels">
     <small id="extensionHelp" class="form-text text-muted">e.g. if base_url is 'google.com/' and extension is '/api' then the request will be sent to 'google.com/api'</small>
   </div>
-  <div class="form-group">
-    <label for="method">http method</label>
-    <input type="text" class="form-control" id="method" placeholder="http method, ex. GET">
-  </div>
   <div class="form-check">
-    <label for="JSON_payload">optional JSON payload</label>
-    <textarea class="form-control" id="JSON_payload"></textarea>
+    <label for="JSON_payload">JSON payload (optional)</label>
+    <textarea name="json" class="form-control" id="JSON_payload"></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <input type="submit" class="btn btn-primary" value="Submit"></input>
 </form>
